3f99285f583c23f9388a505b6f91d94533add0bf
Akira Masuda <masuda.akira1014@gmail.com>
2026-05-10 23:20:36 +0900

    support 2026 labels and remove github-config management (#5)
    * support 2026 labels and remove github-config management
    
    * update
    
    * update
    
    * remove github-config managemet

--- terraform/github_tags.tf
+++ terraform/github_tags.tf
@@ -1,27 +1,6 @@
-terraform {
-  required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "~> 6.0"
-    }
-  }
-}
-
-provider "github" {
-  token = var.github_token
-  owner = "go-zen-chu" # Change me
-}
-
-variable "github_token" {
-  type = string
-}
 
 locals {
-  repositories = {
-    "github-config" = {
-      description = "GitHub configuration managed by terraform"
-      visibility  = "public"
-    }
+  task_repos = {
     "tasks" = {
       description  = "My private tasks"
       has_projects = true
@@ -37,7 +16,7 @@ locals {
     "120m" = "f39800"
   }
   flatten_repository_working_time_colors = flatten([
-    for repo, value in local.repositories : [
+    for repo, value in local.task_repos : [
       for time, color in local.working_time_colors : {
         repository = repo
         time       = time
@@ -49,6 +28,7 @@ locals {
     "2023",
     "2024",
     "2025",
+    "2026",
   ]
   month_colors = {
     "01" = "c7402c"
@@ -73,7 +53,7 @@ locals {
     ]
   ])
   flatten_repository_year_month_colors = flatten([
-    # only set for repositories those require year_month
+    # only set for task_repos those require year_month
     for repo in ["tasks"] : [
       for f in local.flatten_year_month_colors : {
         repository = repo
@@ -82,19 +62,6 @@ locals {
       }
     ]
   ])
-}
-
-# Repositories
-resource "github_repository" "repositories" {
-  for_each    = local.repositories
-  name        = each.key
-  description = each.value.description
-  # default visibility to private
-  visibility             = lookup(each.value, "visibility", "private")
-  has_projects           = lookup(each.value, "has_projects", false)
-  has_issues             = true
-  delete_branch_on_merge = true
-  vulnerability_alerts   = true
 }
 
 # Task time labels definitions
